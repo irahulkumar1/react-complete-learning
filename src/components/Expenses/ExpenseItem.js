a4acf6b80c7d5689bb4b124b5c3624aff7d1bb7a
Rahul Kumar <irahulkv@outlook.com>
2022-02-25 07:44:45 +0530

    folder structure

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,12 +5,16 @@ import Card from '../UI/Cards'
 
 function ExpenseItem(props) {
 
+    const clickHandler = () => {
+        { console.log('clicked') }
+    }
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description" >
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">$ {props.amount}</div>
+                <button onClick={clickHandler}>change title</button>
             </div>
         </Card>
     )
